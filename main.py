2d04b998244a91f7ae761f5345a68d0334c03d92
Mateusz <giszters@gmail.com>
2021-05-30 18:53:35 +0000

    + added date check Use fastkml instead of xml library.

--- main.py
+++ main.py
@@ -1,77 +1,64 @@
 #!/usr/bin/env python
-import xml.etree.ElementTree as ET
 import datetime as dt
-from shapely.geometry import Polygon
+from shapely.geometry import LinearRing
+from fastkml import kml
 
 
 class SearchPosition:
     """SearchPosition is Polygon with date."""
-    def __init__(self, area, date):
-        self.polygon = area
+
+    def __init__(self, geometry, date):
+        self.geometry = geometry
         self.date = date
 
     def __str__(self):
-        return f'SearchPosition({self.date}, {self.polygon})'
+        return f'SearchPosition({self.date}, {self.geometry})'
 
 
 def get_usr_request():
-    area = str2polygon('55.58734,-72.26807,0 47.12514,-67.57458,0 38.84882,-59.56221,0 45.44252,-58.01071,0 55.15113,-65.58617,0 64.48255,-69.85429,0 55.58734,-72.26807,0')
-    return SearchPosition(area, None)
-
-
-def placemark2position(pmark):
-    tspan = pmark.findall('.//{http://www.opengis.net/kml/2.2}name')
-    if len(tspan) != 1:
-        raise Exception('Too many names')
-    date_valid = dt.datetime.strptime(tspan[0].text, '%Y-%m-%dT%H:%M:%S')
-
-    lring = pmark.findall('.//{http://www.opengis.net/kml/2.2}LinearRing')
-    if len(lring) != 1:
-        raise Exception('Too many LinearRings')
-    coords = lring[0].find('.//{http://www.opengis.net/kml/2.2}coordinates').text
-
-    return SearchPosition(str2polygon(coords), date_valid)
-
-
-def str2point(position_s):
-    lon, lat, z = position_s.split(',')
-    if z != '0':
-        raise Exception(f'Were above the ground at level {z}!')
-    return float(lon), float(lat)
-
-
-def str2polygon(coord_s):
-    points_s = coord_s.split(' ')
-    points = list()
-    for point_s in points_s:
-        points.append(str2point(point_s))
-    return Polygon(points)
+    area = LinearRing(
+        [(55.58734, -72.26807, 0), (47.12514, -67.57458, 0), (38.84882, -59.56221, 0), (45.44252, -58.01071, 0),
+         (55.15113, -65.58617, 0), (64.48255, -69.85429, 0), (55.58734, -72.26807, 0)])
+    return SearchPosition(area, dt.date(2021, 4, 27))
 
 
 def get_positions_from_kml(kml_path):
-    kml = ET.parse(kml_path)
-    root = kml.getroot()
-    pmarks = list()
-    for pmark in root.findall('.//{http://www.opengis.net/kml/2.2}Placemark'):
-        pmarks.append(placemark2position(pmark))
-    return pmarks
+    """Read kml file with Acquisition Segments"""
+    k = kml.KML()
+    with open(kml_path, 'r') as f:
+        kml_s = f.read()
+    k.from_string(kml_s.encode('utf-8'))
+    return get_positions(k)
+
+
+def get_positions(elem):
+    """Find all positions in kml structure"""
+    if isinstance(elem, kml.Placemark):
+        return [SearchPosition(elem.geometry, elem.begin)]
+    else:
+        ft = list()
+        for feat in elem.features():
+            ft.extend(get_positions(feat))
+        return ft
 
 
 def is_matching(request, position):
-    #if request.area.intersects(position.area) and request.date == position.date:
-    if request.polygon.intersects(position.polygon):
+    if (request.geometry.intersects(position.geometry) and
+            request.date.year == position.date.year and
+            request.date.month == position.date.month and
+            request.date.day == position.date.day):
         return True
 
 
 def main():
     request = get_usr_request()
 
-    kml_path = "../Sentinel-1A_MP_20210427T160000_20210517T180000.kml"
+    kml_path = '../Sentinel-1A_MP_20210427T160000_20210517T180000.kml'
     available = get_positions_from_kml(kml_path)
 
     for position in available:
         if is_matching(request, position):
-            print('Got one!')
+            print(f'Match: {position}')
 
 
 if __name__ == '__main__':
